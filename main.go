6e5d8ccaf00c27b117247555a9dbddb722e90bb4
arslan.kubanov@playtech.com <arslan.kubanov>
2020-07-12 18:12:28 +0300

    fix

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
     "github.com/influxdata/influxdb1-client/v2"
     "fmt"
     "io/ioutil"
-    "os"
+//    "os"
     "encoding/json"
     "net/http"
 )
+
+
+func read_json(config string) map[string]interface{} {
+    var result map[string]interface{}
+
+    FileJson, err := ioutil.ReadFile(config)
+    if err != nil {
+        panic(err.Error())
+    }
+    json.Unmarshal([]byte(FileJson), &result)
+
+    queries := result["queries"].(map[string]interface{})
+    return queries
+}
 
 
 func get_query(host, database, query string) string {
@@ -19,12 +33,12 @@ func get_query(host, database, query string) string {
             Addr: "http://" + host,
     })
     if err != nil {
-        return err.Error()
+        panic(err.Error())
     }
     q := client.NewQuery(query, database, "")
     response, err := c.Query(q)
     if err !=nil {
-        return err.Error()
+        panic(err.Error())
     }
     if len(response.Results[0].Series) != 0 {
         values := response.Results[0].Series[0].Values
@@ -32,40 +46,46 @@ func get_query(host, database, query string) string {
             value := values[i]
 	    val = fmt.Sprint(value[1])
         }
+
     }
     return val
 }
 
 
 func main() {
-    var host string
-    var database string
-    var config string
-    var listen string
-    var result map[string]map[string]string
 
+    var (
+        host string
+        config string
+        listen string
+    )
 
     flag.StringVar(&host, "host", "localhost:8086", "host to influxdb")
-    flag.StringVar(&database, "database", "", "database to influxdb")
     flag.StringVar(&config, "config", "", "config with query to influxdb")
     flag.StringVar(&listen, "listen", "", "listen address")
 
     flag.Parse()
-
-    jsonFile, err := os.Open(config)
-    defer jsonFile.Close()
-
-    if err != nil {
-        return
-    }
-    byteValue, _ := ioutil.ReadAll(jsonFile)
-    json.Unmarshal([]byte(byteValue), &result)
-    http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-        for key := range result["queries"] {
-            query := result["queries"][key]
-            get_query(host, database, query)
-	    fmt.Fprintf(w, key + "\t" + get_query(host, database, query) + "\n")
+    queries := read_json(config)
+    var responseText string
+    message := func(w http.ResponseWriter, r *http.Request) {
+        for database, _ := range queries {
+            query_json := queries[database].(map[string]interface{})
+	    for query := range query_json {
+	        query_str := fmt.Sprintf("%v", query_json[query])
+		responseText += fmt.Sprintf("influxdb_result_query{database=\"%s\", query_name=\"%s\"} %s\n", database, query, get_query(host, database, query_str))
+	    }
+        fmt.Fprintf(w, responseText)
+        responseText = ""
         }
-    })
+    }
+    http.HandleFunc("/metrics", message)
     http.ListenAndServe(listen, nil)
+//    http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+//    for key := range result["queries"] {
+//        query := result["queries"][key]
+//        get_query(host, database, query)
+//        fmt.Fprintf(w, key + "\t" + get_query(host, database, query) + "\n")
+//        }
+//    })
+//    http.ListenAndServe(listen, nil)
 }
